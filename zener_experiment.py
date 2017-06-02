e60ff4cba2d27fb5cdcef0f83817a58ef52a6e01
gitansh95 <gitansh95@gmail.com>
2017-06-02 12:09:08 +0530

    added/removed functions, making code more understandable to the readed

--- zener_experiment.py
+++ zener_experiment.py
@@ -1,31 +1,83 @@
 from complex_functions import *
 
-def run(Sample, Sample_Box, V_range_array, V_step_array, set_point_array, sample_description, address):
-    zener_run(Sample, Sample_Box, V_range_array, V_step_array, set_point_array, sample_description, address)
+TEST_OBJECTS = ["Insert_RT_Puck", "Puck_Board"]
+
+def run (Sample, Sample_Box, sample_description, address):
+        
+    mount_sample_on_puck (Sample, Sample_Box)
+    
+    print "\n\n Available Test Objects : "
+    print "______________________________\n"
+    
+    for item in TEST_OBJECTS:
+        print item
+    
+    print "______________________________\n"
+    test_object = raw_input ("Load Sample to which test_object? : \n")
+    
+    while (test_object not in TEST_OBJECTS):
+        
+        print "\n\n Available Test Objects : "
+        print "______________________________\n"
+        
+        for item in TEST_OBJECTS:
+            print item
+        
+        print "______________________________\n"
+        test_object = raw_input ("Load Sample to which test_object? : \n")
+    
+    load_sample (Sample, Sample_Box, test_object)
+    
+    print("\nGenerating procedural steps for experiment.  .  .  .\n")
+    
+    switch_on_PQMS_modules()
+    switch_on_computer()
+    set_save_folder(Sample_Box, Sample, sample_description, address)
+    set_up_PQMS_modules()
+    
+    while (True):
+        
+        temperature_set_point, V_range, V_step, I_range, I_step, max_power = get_experimental_parameters()
+        PQMS_IV_run (temperature_set_point, V_range, V_step, I_range, I_step, max_power)
+        response = raw_input("Do you want to do another run? : y/n \n")
+        
+        if (response == 'n'):
+            break
+    
+    switch_off_PQMS_modules()
+    
+    print("\nProcedure has been created. Filename : " + log)
+    print ("\nReady for execution.\n")
+        
+    unload_sample (Sample, Sample_Box, test_object)
+    unmount_sample_from_puck (Sample, Sample_Box)
+    
+
+###############################################################################
 
 
-def zener_run(Sample, Sample_Box, V_range_array, V_step_array, set_point_array, sample_description, address):
-     print("Generating procedural steps for experiment.  .  .  .")
-     load_sample(Sample, Sample_Box)
-     switch_on_PQMS_modules()
-     switch_on_computer()
-     set_save_folder(Sample_Box, Sample, sample_description, address)
-     set_up_PQMS_modules()
-     
-     for i in range(len(V_range_array)):
-         write("##############################################################")
-         write("                   Run "  + str(i+1) + " starts\n")
-         write("##############################################################")
-         
-         configure_XTCON(set_point_array[i])
-         start_IV_run(V_range_array[i], V_step_array[i])
-         
-         write("##############################################################")
-         write("                   Run: " + str(i+1) + " ends\n")
-         write("##############################################################")
-     
-     switch_off_PQMS_modules()
-     unload_sample(Sample, Sample_Box)
-     
-     print("Procedure has been created. Filename : " + log)
-     print ("Ready for execution.")
+def get_experimental_parameters():
+    V_range                    = raw_input("Enter Voltage Sweep Max (mV) : \n")
+    V_step                   = raw_input("Enter Voltage Step Size (mV) : \n")
+    I_range                    = raw_input("Enter Current Sweep Max (uA) : \n")
+    I_step                   = raw_input("Enter Current Step Size (uA) : \n")
+    max_power                    = raw_input("Enter Max Power (mW): \n")
+    temperature_set_point           = raw_input("Enter Heater Setpoint Temperature (K) : \n")
+
+    return temperature_set_point, V_range, V_step, I_range, I_step, max_power
+
+
+def PQMS_IV_run (temperature_set_point, V_range, V_step, I_range, I_step, max_power):
+    
+    write("\n##############################################################")
+    write("                   Run starts")
+    write("##############################################################\n")
+    
+    configure_XTCON(temperature_set_point)
+    start_IV_run(V_range, V_step, I_range, I_step, max_power)
+    
+    write("\n##############################################################")
+    write("                   Run ends")
+    write("##############################################################\n")
+
+            
