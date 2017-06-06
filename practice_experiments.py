3b58102e2c11d2e1bb04c599c0b26fb486d67382
Vishnu Raghuraman <vishnu.raman@gmail.com>
2017-06-06 11:07:51 +0530

    Refined the function

--- practice_experiments.py
+++ practice_experiments.py
@@ -2,14 +2,17 @@ from primitive_functions import *
 
 def run(Sample, Sample_Box, sample_description, address):
   remove(Sample_Box+".Cap", Sample_Box)
-  hold_sample("Zener_1",Sample_Box)
+  leave(Sample_Box)
+  hold_sample(Sample,Sample_Box)
   close_lid(Sample_Box)
-  leave("Zener_2")
+  leave(Sample)
   move("Polyvinyl_Alcohol", "Puck_Screwing_Coordinates")
   leave("Polyvinyl_Alcohol")
   move("Test_Tube_1", "Puck_Board_Home_Coordinates")
   leave("Test_Tube_1")
   move("Polyvinyl_Alcohol", "Test_Tube_1.Home_Coordinates")
-  
+  leave("Polyvinyl_Alcohol")
+  goto("Tweezers.Home_Coordinates")
+  leave("Tweezers")
   
     
