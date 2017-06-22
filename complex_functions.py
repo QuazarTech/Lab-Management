19657748322dbd885cb78a84d66093c8e674dea3
Vishnu Raghuraman <vishnu.raman@gmail.com>
2017-06-22 23:40:27 +0530

    Modified the step ramp functions to use the user defined values for the stabilization delays, monitoring period instead of hardcoding it

--- complex_functions.py
+++ complex_functions.py
@@ -867,7 +867,7 @@ def is_XL_run_needed(final_temperature, ramp_rate, max_depth, step_size, amplitu
 ###############################################################################
 #IV_step_ramp functions functions
 
-def set_IV_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, V_step, I_range, I_step, max_power):
+def set_IV_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, V_step, I_range, I_step, max_power, pre_stabilization_delay, post_stabilization_delay, monitoring_period, tolerance):
     
     move_cursor ('Run control')
     click       ('drop down menu')
@@ -883,10 +883,10 @@ def set_IV_step_ramp_measurement_settings(initial_temperature, final_temperature
     write       ("execute : Set Final Temperature to " + str(final_temperature) + " K")
     write       ("execute : Temperature Step to " + str(temperature_step) + " K")
     
-    write       ("execute : Set Pre-stabilization Delay to 100 seconds")
-    write       ("execute : Set Post-stabilization Delay to 100 seconds")
-    write       ("execute : Set Temperature Tolerance to 0.5 K")
-    write       ("execute : Set Monitoring Period to 300 seconds")
+    write       ("execute : Set Pre-stabilization Delay to " + str(pre_stabilization_delay))
+    write       ("execute : Set Post-stabilization Delay to " + str(post_stabilization_delay))
+    write       ("execute : Set Temperature Tolerance to " + str(tolerance))
+    write       ("execute : Set Monitoring Period to " + str(monitoring_period))
     
     ################################
     click       ('File->Apply')
@@ -905,13 +905,13 @@ def set_IV_step_ramp_measurement_settings(initial_temperature, final_temperature
     click       ("\'File->Done\'")    
 
 
-def start_IV_step_ramp_run (initial_temperature, final_temperature, temperature_step, V_range, V_step, I_range, I_step, max_power):
+def start_IV_step_ramp_run (initial_temperature, final_temperature, temperature_step, V_range, V_step, I_range, I_step, max_power, pre_stabilization_delay, post_stabilization_delay, monitoring_period, tolerance):
     
     goto        ("Qrius Main Window")
     click       ('Measurement Mode settings')
     click       ('Electrical DC Conductivity')
     
-    set_IV_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, V_step, I_range, I_step, max_power)
+    set_IV_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, V_step, I_range, I_step, max_power, pre_stabilization_delay, post_stabilization_delay, monitoring_period, tolerance)
     write("Update_Database Lab_Space,PQMS,XSMU,Mode,I-V")
     write("Update_Database Lab_Space,PQMS,XTCON,Mode,Stepped_Ramp")
     
@@ -925,7 +925,7 @@ def start_IV_step_ramp_run (initial_temperature, final_temperature, temperature_
 ###############################################################################
 # RT_stepped ramp functions
 
-def set_RT_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, I_max, power):
+def set_RT_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, I_range, max_power, pre_stabilization_delay, post_stabilization_delay, monitoring_period, tolerance):
     
     move_cursor ('Run control')
     click       ('drop down menu')
@@ -941,10 +941,10 @@ def set_RT_step_ramp_measurement_settings(initial_temperature, final_temperature
     write       ("execute : Set Final Temperature to " + str(final_temperature) + " K")
     write       ("execute : Temperature Step to " + str(temperature_step) + " K")
     
-    write       ("execute : Set Pre-stabilization Delay to 100 seconds")
-    write       ("execute : Set Post-stabilization Delay to 100 seconds")
-    write       ("execute : Set Temperature Tolerance to 0.5 K")
-    write       ("execute : Set Monitoring Period to 300 seconds")
+    write       ("execute : Set Pre-stabilization Delay to " + str(pre_stabilization_delay))
+    write       ("execute : Set Post-stabilization Delay to " + str(post_stabilization_delay))
+    write       ("execute : Set Temperature Tolerance to " + str(tolerance))
+    write       ("execute : Set Monitoring Period to " + str(monitoring_period))
     
     ################################
     click       ('File->Apply')
@@ -961,13 +961,13 @@ def set_RT_step_ramp_measurement_settings(initial_temperature, final_temperature
     click       ("\'File->Done\'")    
 
 
-def start_RT_step_ramp_run (initial_temperature, final_temperature, temperature_step, V_range, I_max, power):
+def start_RT_step_ramp_run (initial_temperature, final_temperature, temperature_step, V_range, I_range, max_power, pre_stabilization_delay, post_stabilization_delay, monitoring_period, tolerance):
     
     goto        ("Qrius Main Window")
     click       ('Measurement Mode settings')
     click       ('Electrical DC Conductivity')
     
-    set_RT_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, I_max, power)
+    set_RT_step_ramp_measurement_settings(initial_temperature, final_temperature, temperature_step, V_range, I_range, max_power, pre_stabilization_delay, post_stabilization_delay, monitoring_period, tolerance)
     write("Update_Database Lab_Space,PQMS,XSMU,Mode,R-T")
     write("Update_Database Lab_Space,PQMS,XTCON,Mode,Stepped_Ramp")
     
