d158d068e1ea37e1d7e96a2463180e38a39ae27c
Vishnu Raghuraman <vishnu.raman@gmail.com>
2017-06-22 11:28:46 +0530

    Removed the array_of_keys array

--- lab_reset.py
+++ lab_reset.py
@@ -1,6 +1,6 @@
 import yaml
 
-array_of_keys     = []
+
 def lab_reset(folder_name):
 
 	with open(folder_name+"/run_data_new_database.yaml", "r") as f:
@@ -23,8 +23,6 @@ def lab_reset(folder_name):
 def get_differences(d, val_array):
     for k,v in d.items():
             try:
-                array_of_keys.append(k)
                 get_differences(v , val_array)
             except AttributeError:
-                property_key = array_of_keys[-2]
-                val_array.append(property_key + " " + k + "," + str(v))
+                val_array.append(k + "," + str(v))
