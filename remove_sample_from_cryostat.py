9237c8dec59a24cf729101e9c851bba4998cb794
gitansh95 <gitansh95@gmail.com>
2017-06-24 14:38:22 +0530

    added steps to properly disconnect cables, remove vaccum/helium pipes

--- remove_sample_from_cryostat.py
+++ remove_sample_from_cryostat.py
@@ -11,40 +11,39 @@ def run (Sample, Sample_Box, sample_description, address):
     
 	test_object = select_test_object()
         cryostat    = select_cryostat()
-    
-        prepare_sample (Sample, Sample_Box, test_object)
-    
 
 	#####################
 	#disconnecting cables
-
-	disconnect_cable ("HT_Cable")
-	disconnect_cable ("RT_Cable")
-	
+        write   ("execute : Ensure that the cryostat is at room temp (read temperature controller screen). If cold, abort experiment. If hot, set TCON heater setpoint to 300 K and start isothermal.")
+        write   ("execute : Ensure that the cryostat is at atmospheric pressure (read pirani guage screen), If not, open Pump.Release_Valve")
+	cables_disconnected_check (test_object, cryostat)
     
 	#####################    
 	
 	#removing insert
 	
-	unload_sample(Sample, Sample_Box, test_object, cryostat)
+	unclamp()
+	write  ("execute : remove insert from the cryostat")
+	write  ("execute : Leave insert on table")
 
 	##################### 
 
 	#removing cryostat to extract sample
-	write   ("execute : Keep a soft tissue paper on the table near the "+ cryostat + "crysotat")
-	write   ("execute : Unscrew the " + cryostat + " cryostat nuts")
-	write   ("execute : Remove the " + cryostat + " cryostat from the frame and take it to the table ")
-	write   ("execute : Tilt the " + cryostat + " cryostat keeping the mouth over the tissue")
-	write   ("execute : Keep the " + cryostat + " cryostat back in its coordinates")
+	write  ("execute : Keep a soft tissue paper on the table near the "+ cryostat + "crysotat")
+	write  ("execute : Remove all helium/vaccum connections from the cryostat")
+	write  ("execute : Unscrew the " + cryostat + " cryostat nuts that screw it to the PQMS frame")
+	write  ("execute : Pull the " + cryostat + " cryostat upwards from the frame")
+	write  ("execute : Take cryostat to the table ")
+	write  ("execute : Tilt the " + cryostat + " cryostat keeping its mouth over the tissue")
+	write  ("execute : Replace the " + cryostat + " cryostat back to its frame")
 	
 	#####################    
 	
 	#storing sample
 	write   ("execute : Wear gloves to handle the sample ")
-	write   ("execute : Locate tweezers and hold it")
-	write   ("execute : Locate sample cover, hold it and come back to the table")
+	write   ("execute : Hold tweezers")
 	write   ("execute : Use the tweezers to put the sample in its cover ")
-	write   ("execute : Store the cover in the Dessicant ")
+	write   ("execute : Store the cover along with sample in the Dessicant ")
 
 	#####################    
 	
