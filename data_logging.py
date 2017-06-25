dbee4fd8d253409eab86088eb3fdc1aba6b58ec1
Vishnu Raghuraman <vishnu.raman@gmail.com>
2017-06-25 15:34:37 +0530

    Removed some basic compilation errors, and removed the put_in_folder and time_in_ist functions, and added reason for abortion of program

--- data_logging.py
+++ data_logging.py
@@ -1,13 +1,11 @@
 import time
 from pylab import *
-from pytz import timezone
-import datetime
-import time
+from lab_reset import *
 import yaml as yaml
 import sys
 import os
 
-time_zone = timezone("Asia/Kolkata")
+
 time_array = []
 
 def get_user_data():
@@ -15,11 +13,6 @@ def get_user_data():
     sensor = raw_input("\nEnter sensor ID: \n")
     observer = raw_input("\nEnter observer ID: \n")
     return robot, sensor, observer
-
-def time_in_ist(param):
-    now_utc   = datetime.datetime.now(timezone("UTC"))
-    now_india = now_utc.astimezone(time_zone)
-    return now_india.strftime(param)
     
 database = "lab_database"
 robot,sensor,observer = get_user_data()
@@ -88,12 +81,14 @@ def print_states (log, line, new_dbase):
 
 #####################################################################
 
-def abort_execution(log, temp):
+def abort_execution(line, log, temp):
 	print temp
         string = "***********" + "\n" + "EXECUTION ABORTED" + "\n" + "***********"
+        reason = raw_input("What is the reason for aborting the execution?\n")
 	with open (log.name, "a") as log:
         	log.write(string + '\n')
 		log.write("Execution has come to an end due to error in line: " + line + '\n')
+		log.write("ERROR - " + reason + '\n')
 	log.close()
 	return string
 
@@ -101,9 +96,11 @@ def abort_execution(log, temp):
 def pause_execution(log, temp):
 	print temp
         string = "***********" + "\n" + "EXPERIMENT PAUSED" + "\n" + "***********"
+        reason = raw_input("Why has the execution been paused?\n")
 	with open (log.name, "a") as log:
 		log.write(string + '\n')
 		log.write("Execution has been paused at: " + temp + '\n')
+		log.write("PAUSE - " + reason + '\n')
 	log.close()
 
 	response = raw_input("Type 'resume' to resume the experiment :")
@@ -116,18 +113,7 @@ def pause_execution(log, temp):
 		log.write("Execution has been resumed at: " + temp + '\n')
 	log.close()
 	
-#####################################################################	
-
-def put_in_folder (experiment, log, diff_file, new_dbase):        
-	t = time_in_ist('%Y_%m_%d_%H:%M:%S')
-	folder_name = "run_data_" + experiment.name + "_" + t
-	os.system("mkdir " + folder_name)
-	os.system("mv " + experiment.procedure + " " + folder_name + "/")
-	os.system("mv " + log + " " + folder_name + "/")
-	os.system("mv " + diff_file + " " + folder_name + "/")
-	os.system("mv " + new_dbase + ".yaml " + folder_name + "/")
-	lab_reset.lab_reset(folder_name)
-	create_duration_log (experiment, folder_name)
+#####################################################################
 
 def time_stamp_and_comments(log, line, temp, user_input):
 	with open (log.name, "a") as log:
@@ -146,56 +132,37 @@ def initialize_database(database_name):
     		fbase.close()
     		f.close()
 
-def create_duration_log (experiment, folder_name):
-	f = open(folder_name + "/" + experiment.procedure, "r")
-	fnew = open(folder_name + "/duration_log.txt", "w")
-	fnew.write ("PQMS Data Run Time\n\n")
-	i = 0
-	for line in f:
-		if (line == 'execute : Wait till sample temperature stabilizes\n'):
-			a = str(time_array[i]) + "-" + str(time_array[i+1])
-			fnew.write("Temperature Stabilization time:" + str(a) + "\n\n")
-		elif (line == 'execute : Wait until graph comes to an end\n'):
-			a = str(time_array[i]) + "-" + str(time_array[i+1])
-			fnew.write("I_V run time:" + str(a) + "\n\n")
-	        if(i == len(time_array) - 1):
-	        	break
-	 	i += 1 
- 	fnew.close()
- 	f.close()
 
 def write(line):
-  log = open ("run_data_execution_log.txt", "a")
-  if (line[0:7] != 'execute'):
-      if(line[0:6] == 'Update'):
-	  update_database(line, diff_file, new_dbase)
-      else :
-	  print(line)
-	  with open (log.name, "a") as log:
-		  log.write(line + '\n')
-		  log.close()
+	log = open ("run_data_execution_log.txt", "a")
+	if (line[0:7] != 'execute'):
+		if(line[0:6] == 'Update'):
+			update_database(line, diff_file, new_dbase)
+		else :
+			print(line)
+			with open (log.name, "a") as log:
+				log.write(line + '\n')
+				log.close()
 
-  else:
-      if(line[10:14] != 'Read'):
-	  print(line + '\n')
-	  user_input = raw_input("Comments, if any : (Press Enter to continue, type 'pause' to pause or 'end' to abort) : ")
-	  temp = time_in_ist('%H:%M:%S')
-	  
-	  if (user_input == "end"):
-	      string = abort_execution(log, temp)
-	      put_in_folder(log.name, diff_file, new_dbase)
-	      sys.exit(string)
-	  
-	  elif (user_input == "pause"):
-	      pause_execution(log, temp)
-	      
-	  else:
-	      print temp + '\n'
+	else:
+		if(line[10:14] != 'Read'):
+			print(line + '\n')
+			user_input = raw_input("Comments, if any : (Press Enter to continue, type 'pause' to pause or 'end' to abort) : ")
+			temp = time_in_ist('%H:%M:%S')
+			if (user_input == "end"):
+				string = abort_execution(line, log, temp)
+				sys.exit(string)
 
-	  time_array.append (time_in_ist('%H:%M:%S'))
-	  time_stamp_and_comments(log, line, temp, user_input)
+			elif (user_input == "pause"):
+				pause_execution(log, temp)
 
-      else:
-	  print_states(log, line, new_dbase)
+			else:
+				print temp + '\n'
 
+			time_array.append (time_in_ist('%H:%M:%S'))
+			time_stamp_and_comments(log, line, temp, user_input)
+
+		else:
+			print_states(log, line, new_dbase)
+	
 initialize_database (database)
