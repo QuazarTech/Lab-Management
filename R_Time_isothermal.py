d0290743aae8fbe90a08ede3d7235e961506cb21
Vishnu Raghuraman <vishnu.raman@gmail.com>
2017-06-25 10:57:27 +0530

    Called get_sample_info() inside the experiment

--- R_Time_isothermal.py
+++ R_Time_isothermal.py
@@ -3,9 +3,9 @@ from wrapper_functions import *
 
 name = "R_Time_isothermal"
 
-def run (Sample, Sample_Box, sample_description, address):
+def run ():
     
-    
+    address, Sample, Sample_Box, sample_description = get_sample_info()
     test_object = select_test_object()
     prepare_sample (Sample, Sample_Box, test_object)
     
