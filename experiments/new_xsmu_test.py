0b6d555d898a424bac157a35873bfb6e0a84576f
gitansh95 <gitansh95@gmail.com>
2017-07-14 14:15:39 +0530

    added temperature control to the procedure

--- experiments/new_xsmu_test.py
+++ experiments/new_xsmu_test.py
@@ -14,7 +14,22 @@ def run():
     goto ('Top Left Corner of the screen')
     click ('Power Button -> Service Engineer')
     write ('execute : Enter password')
+   
+    goto  ("Qrius")
+    click ("Settings->Global Settings")
+    write ("execute : Set TCON Serial Number to 19A")
+    click ("OK")
+    click ("MOdules Manager -> Temperature Controller")
+    goto  ("Run Control")
+    click ("Drop Down Menu")
+    click ("Isothermal")
+    click ("Settings->Isothermal Settings")
+    write ("execute :Set Isothermal Setpoint to 318K")
+    click ("File->Apply")
+    click ("Start")
     
+    write ("execute : Wait for about 1 hour for temperature to stabilize. Press Enter when wait is over.")
+   
     write ('execute : Open Terminal')
     goto  ('${HOME}')
     write ('execute : type python VM_getReading_3.py')
