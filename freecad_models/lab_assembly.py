d4c5578235bb2ae5eee2516a7982a1724aeb0790
gitansh95 <gitansh95@gmail.com>
2017-08-14 17:35:11 +0530

    Modified erroneous comments

--- freecad_models/lab_assembly.py
+++ freecad_models/lab_assembly.py
@@ -13,14 +13,14 @@ Gui.ActiveDocument = Gui.getDocument(assembly_file.Name)
 ###############################################################################
 ## Import all parts into the assembly
 
-# import cover_bottom
+# import Computer_Table Assembly
 computer_assembly = importPart.importPart(filename = 'models/Computer_Assembly.fcstd', partName = None, doc_assembly = assembly_file)
 
 App.ActiveDocument.recompute()
 Gui.SendMsgToActiveView("ViewFit")
 Gui.activeDocument().activeView().viewAxonometric()
 
-# import cover_bottom
+# import EM_coupling_experiment Assembly
 EM_coupling_experiment = importPart.importPart(filename = 'models/experiment_assembly.fcstd', partName = None, doc_assembly = assembly_file)
 
 App.ActiveDocument.recompute()
@@ -31,14 +31,13 @@ Gui.activeDocument().activeView().viewAxonometric()
 raw_input('All parts imported. Press Enter to assemble the experiment_assembly on the computer table.')
 ###############################################################################
 
-# Place the experiment setup on the lockin top surface
+# Place the experiment setup on the table surface
 Gui.Selection.clearSelection()                                   # Clear the selection list, no items selected
 Gui.Selection.addSelection(computer_assembly     , "Face1397")   # Select the face on computer_assembly to create a planar mate on (must be a plane face)
 Gui.Selection.addSelection(EM_coupling_experiment, "Face026")    # Select the face on EM_coupling_experiment to create a planar mate on (must be a plane face)
 
 selection = Gui.Selection.getSelectionEx()                       # Get the selected items from FreeCAD Gui
 planeConstraint.parseSelection(selection, objectToUpdate=None)   # Create a Planar mate on the selected faces
-
 
 # Mate from side
 Gui.Selection.clearSelection()
