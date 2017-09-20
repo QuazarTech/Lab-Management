1c94116e158422323cf9e04e8770a3dc0bd33c67
Vishnu Raghuraman <vishnu.raman@gmail.com>
2017-09-20 20:09:44 +0530

    Completing the file and removing any raw_input commands

--- freecad_models/XPLORE/code/primitive_movements.py
+++ freecad_models/XPLORE/code/primitive_movements.py
@@ -2,14 +2,20 @@
 #
 # This program contains primitives required for performing any step in Lab-Management.
 
-import functools
+
+# Modules necessary for operation
+import sys
 import numpy as np
 import FreeCAD
-import time
+import functools
 from PySide import QtCore
 
+timer   = QtCore.QTimer()
+timer_1 = QtCore.QTimer()
 # Define a timer object which will have a particular timeout
-timer = QtCore.QTimer()
+
+# The no of animation steps, currently set at 200
+SAMPLES = "200"
 
 ########################################################################################
 ########################################################################################
@@ -17,15 +23,15 @@ timer = QtCore.QTimer()
 
 def get_current_position(obj):
 
-	p = FreeCAD.ActiveDocument.getObject(obj).Placement.Base
+	p = FreeCAD.ActiveDocument.getObjectsByLabel(obj)[0].Placement.Base
 	return p.x, p.y, p.z
 
 def change_position(new_pos, obj):
 
 	new_x, new_y, new_z = new_pos
-	FreeCAD.ActiveDocument.getObject(obj).Placement.Base.x = new_x
-	FreeCAD.ActiveDocument.getObject(obj).Placement.Base.y = new_y
-	FreeCAD.ActiveDocument.getObject(obj).Placement.Base.z = new_z
+	FreeCAD.ActiveDocument.getObjectsByLabel(obj)[0].Placement.Base.x = new_x
+	FreeCAD.ActiveDocument.getObjectsByLabel(obj)[0].Placement.Base.y = new_y
+	FreeCAD.ActiveDocument.getObjectsByLabel(obj)[0].Placement.Base.z = new_z
 
 def update_position(initParams):
 
@@ -34,9 +40,7 @@ def update_position(initParams):
 	x_final, y_final, z_final   = final_pos
 	x      , y      , z         = get_current_position(obj)
 
-	if np.abs(x - x_final) < 0.01 \
-	and np.abs(y - y_final) < 0.01 \
-	and np.abs(z - z_final) < 0.01:
+	if np.abs(x - x_final) < 0.1 and np.abs(y - y_final) < 0.1 and np.abs(z - z_final) < 0.1:
 		timer.stop()
 
 	change_position(( x + unit_x ,\
@@ -55,12 +59,10 @@ def get_units(obj, final_pos, samples):
 
 	return unit
 
-def move():
-	obj       = raw_input("Enter the object you wanna move in this Active Document:\n")
-	samples   = raw_input("Enter the unit steps to move:\n")
-	final_x   = raw_input("Enter the final x:\n")
-	final_y   = raw_input("Enter the final y:\n")
-	final_z   = raw_input("Enter the final z:\n")
+def translate(obj, final_x, final_y, final_z, samples):
+
+	
+	x_current, y_current, z_current = get_current_position(obj)
 
 	final_pos = (float(final_x), float(final_y), float(final_z))
 	units     = get_units(obj, final_pos ,float(samples))
@@ -75,12 +77,12 @@ def move():
 #Rotation Functions
 
 def get_current_euler_angles(obj):
-	p = FreeCAD.ActiveDocument.getObject(obj).Placement.Rotation
+	p = FreeCAD.ActiveDocument.getObjectsByLabel(obj)[0].Placement.Rotation
 	return p.toEuler()
 
 def rotate_along_direction (obj, roll_x, yaw_z, pitch_y):
 	rot = FreeCAD.Rotation(yaw_z, pitch_y, roll_x)
-	FreeCAD.ActiveDocument.getObject(obj).Placement.Rotation = rot
+	FreeCAD.ActiveDocument.getObjectsByLabel(obj)[0].Placement.Rotation = rot
 
 def update_rotation (initParams):
 	obj, final_angles, unit                  = initParams
@@ -89,7 +91,7 @@ def update_rotation (initParams):
 	roll_x, yaw_z, pitch_y                   = final_angles
 
 	if np.abs(roll_current  - roll_x ) < 0.1 and np.abs(yaw_current - yaw_z) < 0.1 and np.abs(roll_current - roll_x) < 0.1:
-		timer.stop()
+		timer_1.stop()
 	
 	rotate_along_direction(obj, (roll_current  + unit_roll),\
 	 (yaw_current + unit_yaw), (pitch_current + unit_pitch))
@@ -112,21 +114,15 @@ def get_units_rotation(obj, samples, final_angles):
 
 	return (unit_roll, unit_pitch, unit_yaw)
 
-def rotate():
-	obj        = raw_input("Enter the object you wanna move in this Active Document:\n")
-	x_rotation = raw_input("Enter the x rotation (roll):\n")
-	y_rotation = raw_input("Enter the y rotation (pitch):\n")
-	z_rotation = raw_input("Enter the z rotation (yaw):\n")
-	samples    = raw_input("Enter the no of steps:\n")
+def rotate(obj, x_rotation, y_rotation, z_rotation, samples):
 
+	
 	final_angles = (float(x_rotation), float(z_rotation), float(y_rotation))
 	unit    = get_units_rotation(obj, float(samples), final_angles)
 	initParams = (obj, final_angles, unit)
 	timerCallback = functools.partial(update_rotation, initParams)
-	timer.timeout.connect(timerCallback)
-	timer.start(1)
+	timer_1.timeout.connect(timerCallback)
+	timer_1.start(1)
 
 ########################################################################################
 ########################################################################################
-
-rotate()
